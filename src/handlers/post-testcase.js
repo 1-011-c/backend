74f58443b5826f53d96c9660f43e5338016ff71a
nt <nt@hbt.de>
2020-03-21 11:15:04 +0100

    Added new lambda

---
+++ src/handlers/post-testcase.js
@@ -0,0 +1,46 @@
+// Create clients and set shared const values outside of the handler
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk/clients/dynamodb');
+import { v4 as uuidv4 } from 'uuid';
+
+const docClient = new dynamodb.DocumentClient();
+
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.TEST_CASE_TABLE_NAME;
+
+/**
+ * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ */
+exports.postTestcaseHandler = async (event) => {
+    const { body, httpMethod, path } = event;
+    if (httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${httpMethod} method.`);
+    }
+
+    const Item = {
+        "id": uuidv4(), // Technical ID TODO remove this later
+        "uuid_read": uuidv4(),
+        "uuid_write": uuidv4(),
+        "infected": "NOT_TESTED",
+        "date": new Date().toISOString()
+    };
+
+    console.log('Created Test case', JSON.stringify(Item));
+
+    // Creates a new item, or replaces an old item with a new item
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+    const params = {
+        TableName: tableName,
+        Item,
+    };
+    await docClient.put(params).promise();
+
+    const response = {
+        statusCode: 200,
+        body: Item,
+    };
+
+    console.log(`response from: ${path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+};
