ecbb4269c2a5db3f20a9b6777d130d1d5e2c02e3
Klaus Richarz <kr@hbt.de>
2020-03-22 14:17:06 +0100

    revert last change after failed test

--- src/handlers/patch-set-testcase-status.js
+++ src/handlers/patch-set-testcase-status.js
@@ -78,12 +78,6 @@ async function processRequest(uuid, infected, httpMethod) {
     return {
         statusCode,
         body: JSON.stringify(responseBody),
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin" : "*",
-            "Access-Control-Allow-Methods" : "GET, POST, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "*"
-        }
     };
 }
 
